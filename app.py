6d349bc7e659daa7ef6c4c3b7d12cef0f44a9314
Rafael Silva <raparecido@img.com.br>
2024-03-24 19:57:15 -0300

    Alterações nos arquivo restaurante e app, mais a criação do módulo cardápio com seus arquivos item_cardapio, bebida e prato

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from restaurante import Restaurante
+from cardapio.prato import Prato
+from cardapio.bebida import Bebida
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Rafael', 9)
-restaurante_praca.receber_avaliacao('Ana', 4)
-restaurante_praca.receber_avaliacao('Gomes', 10)
-
+bebida_suco = Bebida('Suco de Melância', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
 
 
 def main():
-    Restaurante.listar_restaurante()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
